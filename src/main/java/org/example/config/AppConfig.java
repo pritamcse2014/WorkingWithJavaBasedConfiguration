1fc07ebdafd3d77486541364443a7b8b78b783c9
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-29 21:53:17 +0600

    PrimaryAndQualifier

--- src/main/java/org/example/config/AppConfig.java
+++ src/main/java/org/example/config/AppConfig.java
@@ -3,8 +3,11 @@ package org.example.config;
 import org.example.Alien;
 import org.example.Computer;
 import org.example.Desktop;
+import org.example.Laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
@@ -24,7 +27,8 @@ public class AppConfig {
 //    }
 
     @Bean
-    public Alien alien(Computer computer) {
+//    public Alien alien(@Qualifier("desktop") Computer computer) {
+        public Alien alien(Computer computer) {
         Alien alien = new Alien();
         alien.setAge(25);
         alien.setComputer(computer);
@@ -37,5 +41,11 @@ public class AppConfig {
 //@Bean(name = {"computer2", "desktop", "Beast"})
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
