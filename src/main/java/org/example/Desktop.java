5b71405ea5f03cf1b5cc3129cbbed3151af3704e
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-30 22:55:48 +0600

    ScopeAndValueAnnotation

--- src/main/java/org/example/Desktop.java
+++ src/main/java/org/example/Desktop.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@Component("computer2")
 @Component
 @Primary
+@Scope("prototype")
 public class Desktop implements Computer {
 
     public Desktop() {
